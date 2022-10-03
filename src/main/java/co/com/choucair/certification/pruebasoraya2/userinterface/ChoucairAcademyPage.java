a45fd54a8eb6bde4a33d469d364d5b94e62647cf
sorajy <jyca21@gmail.com>
2022-10-03 07:35:14 -0500

    Se agrega el proyecto

--- src/main/java/co/com/choucair/certification/pruebasoraya2/userinterface/ChoucairAcademyPage.java
+++
@@ -1,9 +0,0 @@
-package co.com.choucair.certification.pruebasoraya2.userinterface;
-
-
-import net.serenitybdd.core.pages.PageObject;
-import net.thucydides.core.annotations.DefaultUrl;
-
-@DefaultUrl("https://operacion.choucairtesting.com/academy/login/index.php")
-public class ChoucairAcademyPage extends PageObject {
-}
